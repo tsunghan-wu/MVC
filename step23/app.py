986272009211c540cee82e58abe05afcc3c08250
Shin Lee <shinying.lees@gmail.com>
2021-06-26 12:34:43 +0800

    Add flexible width

--- step23/app.py
+++ step23/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, jsonify
 
-from config import *
 from imgedit import crop_src_patch, prepare
 from mvc import MVC_Cloner
 
@@ -11,13 +10,7 @@ cloner = MVC_Cloner()
 
 @app.route('/')
 def home():
-    args = {
-        'jpoly_width': JPOLY_WIDTH,
-        'jpoly_height': JPOLY_HEIGHT,
-        'konva_width': KONVA_WIDTH,
-        'konva_height': KONVA_HEIGHT,
-    }
-    return render_template('example.html', **args)
+    return render_template('example.html')
 
 
 @app.route('/crop', methods=['POST'])
