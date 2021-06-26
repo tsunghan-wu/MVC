Fix rotation

step23/imgedit.py
@@ -4,8 +4,6 @@ import os
 import cv2
 import numpy as np
 
-from config import *
-
 
 def fit(img, boundary):
     xmin, ymin = boundary.min(axis=0)
@@ -31,7 +29,7 @@ def crop_poly(img, boundary, max_w, max_h, add_alpha=True, tight=True):
         img = cv2.resize(img, (int(max_h/h*w), max_h))
 
     mask = np.zeros_like(img)
-    mask = cv2.fillPoly(mask, [np.int32(boundary)], (255, 255, 255))
+    mask = cv2.fillPoly(mask, [boundary], (255, 255, 255))
     img = img * mask.astype(bool)
 
     if add_alpha:
@@ -91,7 +89,7 @@ def rotate(src, points, angle, scale=1.):
     ps = points - np.array([w*0.5, h*0.5])
     ps = np.matmul(ps, rot_mat[:,:2].T) + np.array([nw/2, nh/2])
 
-    return img, ps.astype(int)
+    return img, ps.round().astype(int)
 
 
 def prepare(info, dir_path='static/data', src_name='src.png', 
@@ -121,18 +119,18 @@ def prepare(info, dir_path='static/data', src_name='src.png',
     sy = info['src_size']['height'] / crp_shape[1] * ty
     src = cv2.resize(src, None, fx=sx, fy=sy)
 
-    bndry = np.int32(bndry * np.array([sx, sy]))
-    src = crop_poly(src, bndry, src.shape[1], src.shape[0], add_alpha=False, tight=True)
+    bndry = bndry * np.array([sx, sy])
+    src = fit(src, np.int32(bndry.round()))
     bndry = bndry - bndry.min(axis=0)
-    center = np.array(src.shape[:2][::-1]) / 2
 
+    bndry = np.vstack([bndry, [0, 0]])
     patch, bndry = rotate(src, bndry, -info['rot'])
 
-    pos = np.array([info['pos']['x'], info['pos']['y']])
-    pos = pos * np.array([tx, ty])
+    pos = np.array([info['pos']['x'], info['pos']['y']]) * [tx, ty] - bndry[-1] + \
+        np.array([patch.shape[1], patch.shape[0]]) / 2
+    bndry = bndry[:-1]
 
-    M = cv2.getRotationMatrix2D((0, 0), -info['rot'], 1.)
-    center = -np.matmul(-center, M[:,:2].T)
-    pos = np.array([info['pos']['x']*tx, info['pos']['y']*ty]) + center
+    patch = fit(patch, bndry)
+    bndry = bndry - bndry.min(axis=0)
 
-    return patch[:,:,:3].astype(np.uint8), tar, bndry, pos.astype(int)
+    return patch.astype(np.uint8), tar, bndry.astype(int), pos.astype(int)

step23/mvc_bak.py
@@ -6,13 +6,20 @@ class MVC_Cloner:
     def __init__(self):
         self.adaptive_mesh = False
 
-    def preprocess(self, src_image, curve):
+    def preprocess(self, src_img, curve):
         if self.adaptive_mesh is False:
-            src_mask = np.zeros(src_image.shape, src_image.dtype)
+            src_mask = np.zeros_like(src_img)
             cv2.fillPoly(src_mask, [curve], (255, 255, 255))
             src_mask = cv2.cvtColor(src_mask, cv2.COLOR_BGR2GRAY)
+
+            kernel = np.ones((5, 5), np.uint8)
+            src_mask = cv2.erode(src_mask, kernel, iterations=1)
+
+            contours, hier = cv2.findContours(src_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            contours = contours[0].squeeze(1)
             coord = cv2.findNonZero(src_mask).squeeze(axis=1)
-            return coord
+
+            return coord, contours
 
     def rad(self, v1, v2, norm1, norm2):
         cos = (v1 * v2).sum(axis=1) / norm1 / norm2
@@ -38,24 +45,16 @@ class MVC_Cloner:
         return mvc / mvc.sum(axis=1, keepdims=True)
 
 
-    def count_diff(self, curve, translation, src_image, dst_image):
+    def count_diff(self, curve, translation, src_img, dst_img):
         curve_shft = curve + translation
-        D = [dst_image[trg_x[1], trg_x[0]] - src_image[src_x[1], src_x[0]] \
+        D = [dst_img[trg_x[1], trg_x[0]] - src_img[src_x[1], src_x[0]] \
             for src_x, trg_x in zip(curve, curve_shft)]
         
         return np.stack(D)
 
     def process(self, src_img, dst_img, src_center, dst_center, curve):
         translation = (dst_center - src_center)
-        src_x = self.preprocess(src_img, curve)
-
-        # contours
-        src_mask = np.zeros(src_img.shape, src_img.dtype)
-        cv2.fillPoly(src_mask, [curve], (255, 255, 255))
-        src_mask = cv2.cvtColor(src_mask, cv2.COLOR_BGR2GRAY)
-        contours, hier = cv2.findContours(src_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        contours = contours[0].squeeze(1)
-        print('Contours:', contours.shape)
+        src_x, contours = self.preprocess(src_img, curve)
 
         src_img = src_img.astype(np.float) / 255
         dst_img = dst_img.astype(np.float) / 255